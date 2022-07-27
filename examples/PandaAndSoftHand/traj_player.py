a87ee66a6de1472caf4a787b260f3bf3ebfe559f
Seb <sebtiburzio@gmail.com>
2022-07-27 12:09:28 +0200

    Update traj_player using ineterpolation code from traj_recorder

--- examples/PandaAndSoftHand/traj_player.py
+++ examples/PandaAndSoftHand/traj_player.py
@@ -21,67 +21,58 @@ def pose_callback(pose):
     global pose_current 
     pose_current = pose
 
-# Adapted from Learning_from_demonstration.py go_to_pose() TODO - maybe refactor a bit
-def go_to_pose(goal_pose):
-    start = np.array([pose_current.pose.position.x, pose_current.pose.position.y, pose_current.pose.position.z])
-    start_ori = np.array([pose_current.pose.orientation.w, pose_current.pose.orientation.x, pose_current.pose.orientation.y, pose_current.pose.orientation.z])
-    goal_ = np.array([goal_pose.pose.position.x, goal_pose.pose.position.y, goal_pose.pose.position.z])
-    # interpolate from start to goal with attractor distance of approx 10 cm (changed from 1cm to account for slower record rate 10Hz)
-    squared_dist = np.sum(np.subtract(start, goal_)**2, axis=0)
+# Adapted from Learning_from_demonstration.py go_to_pose()
+def interpolate_poses(pose_start, pose_end, interp_dist):
+    start_pos = np.array([pose_start.pose.position.x, pose_start.pose.position.y, pose_start.pose.position.z])
+    start_quat = np.quaternion(pose_start.pose.orientation.w, pose_start.pose.orientation.x, pose_start.pose.orientation.y, pose_start.pose.orientation.z)
+    goal_pos = np.array([pose_end.pose.position.x, pose_end.pose.position.y, pose_end.pose.position.z])
+    goal_quat = np.quaternion(pose_end.pose.orientation.w, pose_end.pose.orientation.x, pose_end.pose.orientation.y, pose_end.pose.orientation.z)
+
+    # Determine no. of interpolation steps 
+    # Distance steps
+    squared_dist = np.sum(np.subtract(start_pos, goal_pos)**2, axis=0)
     dist = np.sqrt(squared_dist)
-    interp_dist = 0.01  # [m]
+    interp_dist = interp_dist  # [m]
     step_num_lin = math.floor(dist / interp_dist)
-    
-    q_start=np.quaternion(start_ori[0], start_ori[1], start_ori[2], start_ori[3])
-    q_goal=np.quaternion(goal_pose.pose.orientation.w, goal_pose.pose.orientation.x, goal_pose.pose.orientation.y, goal_pose.pose.orientation.z)
-    inner_prod=q_start.x*q_goal.x+q_start.y*q_goal.y+q_start.z*q_goal.z+q_start.w*q_goal.w
+    # Orientation steps
+    inner_prod = start_quat.x*goal_quat.x+start_quat.y*goal_quat.y+start_quat.z*goal_quat.z+start_quat.w*goal_quat.w
     if inner_prod < 0:
-        q_start.x=-q_start.x
-        q_start.y=-q_start.y
-        q_start.z=-q_start.z
-        q_start.w=-q_start.w
-    inner_prod=q_start.x*q_goal.x+q_start.y*q_goal.y+q_start.z*q_goal.z+q_start.w*q_goal.w
-    theta= np.arccos(np.abs(inner_prod))
+        start_quat.x = -start_quat.x
+        start_quat.y = -start_quat.y
+        start_quat.z = -start_quat.z
+        start_quat.w = -start_quat.w
+    inner_prod = start_quat.x*goal_quat.x+start_quat.y*goal_quat.y+start_quat.z*goal_quat.z+start_quat.w*goal_quat.w
+    theta = np.arccos(np.abs(inner_prod))
     interp_dist_polar = 0.01 
     step_num_polar = math.floor(theta / interp_dist_polar)
+    # Use maximum step count
+    step_num = np.max([step_num_polar,step_num_lin])
 
-    step_num=np.max([step_num_polar,step_num_lin])
+    # Construct list of interpolated poses
+    interpolated_traj = pose_start
 
-    x = np.linspace(start[0], goal_pose.pose.position.x, step_num)
-    y = np.linspace(start[1], goal_pose.pose.position.y, step_num)
-    z = np.linspace(start[2], goal_pose.pose.position.z, step_num)
-    
-    goal = PoseStamped()
-    
-    goal.pose.position.x = x[0]
-    goal.pose.position.y = y[0]
-    goal.pose.position.z = z[0]
-    
-    quat=np.slerp_vectorized(q_start, q_goal, 0.0)
-    goal.pose.orientation.x = quat.x
-    goal.pose.orientation.y = quat.y
-    goal.pose.orientation.z = quat.z
-    goal.pose.orientation.w = quat.w
+    interp_x = np.linspace(start_pos[0], goal_pos[0], step_num)
+    interp_y = np.linspace(start_pos[1], goal_pos[1], step_num)
+    interp_z = np.linspace(start_pos[2], goal_pos[2], step_num)
 
-    goal_pub.publish(goal)
+    for i in range(step_num):
+        interp_pose = PoseStamped()      
+        interp_pose.header.seq = 1
+        interp_pose.header.stamp = rospy.Time.now()
+        interp_pose.header.frame_id = ""
 
-    goal = PoseStamped()
-    for i in range(step_num):      
-        goal.header.seq = 1
-        goal.header.stamp = rospy.Time.now()
-        goal.header.frame_id = "map"
+        interp_pose.pose.position.x = interp_x[i]
+        interp_pose.pose.position.y = interp_y[i]
+        interp_pose.pose.position.z = interp_z[i]
+        interp_quat = np.slerp_vectorized(start_quat, goal_quat, i/step_num)
+        interp_pose.pose.orientation.x = interp_quat.x
+        interp_pose.pose.orientation.y = interp_quat.y
+        interp_pose.pose.orientation.z = interp_quat.z
+        interp_pose.pose.orientation.w = interp_quat.w
 
-        goal.pose.position.x = x[i]
-        goal.pose.position.y = y[i]
-        goal.pose.position.z = z[i]
-        quat=np.slerp_vectorized(q_start, q_goal, i/step_num)
-        goal.pose.orientation.x = quat.x
-        goal.pose.orientation.y = quat.y
-        goal.pose.orientation.z = quat.z
-        goal.pose.orientation.w = quat.w
-        goal_pub.publish(goal)
-        record_rate.sleep()
+        interpolated_traj = np.c_[interpolated_traj, interp_pose]
 
+    return interpolated_traj
 
 #%%
 if __name__ == '__main__':
@@ -96,22 +87,31 @@ if __name__ == '__main__':
     key_listener.start()
     key_in = None
 
-    record_rate = rospy.Rate(10)
-
+    record_rate = 10 # Hz
+    ros_record_rate = rospy.Rate(record_rate)
+    
     if len(sys.argv) < 2:
         print("Path to recorded trajectory (relative to script) must be included as an argument.")
-        exit
+        sys.exit()
     record = np.load(str(pathlib.Path().resolve())+'/'+sys.argv[1], allow_pickle=True)
-
     playback_data = record['pose_trajectory'].squeeze()
+
     print("Going to starting pose...")
-    go_to_pose(playback_data[0])
+    traj_to_start = interpolate_poses(pose_current, playback_data[0], 0.1/record_rate).squeeze()
+    for i in range (traj_to_start.size):
+        goal_pose = traj_to_start[i]
+        goal_pub.publish(goal_pose)
+        if key_in == Key.esc:
+            break
+        ros_record_rate.sleep()
+
     print("Playback started. Press Esc to stop.")
     for i in range (playback_data.size):
         goal_pose = playback_data[i]
         goal_pub.publish(goal_pose)
         if key_in == Key.esc:
             break
-        record_rate.sleep()
+        ros_record_rate.sleep()
+
     print("Playback completed.")
     
